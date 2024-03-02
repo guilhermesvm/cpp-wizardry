7b77aef2f137f3e0089b3e64bb4a082c27056a93
Guilherme Machado <guilhermesvmachado@gmail.com>
2024-03-02 10:23:55 -0300

    Adding data structures II files

---
+++ data-structures-II/2 - FileIO/p01.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ifstream arq;       //Input file stream
+    arq.open("numeros.in");     //Abre o arquivo
+    if(!arq){
+        cout << "Could not open.";
+        return 1;
+    }
+
+    int num;
+    while(arq >> num){      //Enquanto houver dados a serem extraídos do stream
+        cout << num << " " << num * 2 << endl;
+    }
+
+    arq.close();    //Fecha o arquivo
+    return 0;
+}
